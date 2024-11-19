c75868e2e18dbba455c9f51a11c8296f5b798d76
Polina Kolesnikova <polina@example.com>
2024-11-19 17:32:18 +0100

    fix: adding guess number game

--- number_guess/number_guess.sh
+++ number_guess/number_guess.sh
@@ -3,15 +3,16 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 GUESSING_GAME() {
   RANDOM_NUMBER=$((1 + $RANDOM % 1000))
+  echo $RANDOM_NUMBER
   echo -e "Guess the secret number between 1 and 1000:"
   read USER_NUMBER
   COUNT_TRIES=1
-  
-  while [[ $USER_NUMBER != $RANDOM_NUMBER ]] 
+
+  while [[ $USER_NUMBER != $RANDOM_NUMBER ]]
     do
       if  [[ $USER_NUMBER =~ ^[0-9]+$ ]]
       then
-    
+        ((COUNT_TRIES++))
         if [[  $USER_NUMBER -lt $RANDOM_NUMBER ]]
         then
           echo "It's lower than that, guess again:"
@@ -23,11 +24,10 @@ GUESSING_GAME() {
         echo -e "That is not an integer, guess again:"
       fi
       read USER_NUMBER
-      ((COUNT_TRIES++)) 
     done
     echo "You guessed it in $COUNT_TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
-    
-    
+
+
 
 }
 START_GAME() {
@@ -42,18 +42,18 @@ START_GAME() {
     RES_INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
   else
      IFS="|" read -r USER_ID USERNAME_db GAMES_PLAYED BEST_GAME <<< $RES_USERNAME
-    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    echo "Welcome back, $USERNAME_db! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
-  
+
   GUESSING_GAME
   (( GAMES_PLAYED++ ))
 
-  $PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE username='$USERNAME'"
+   RES_UPDATE_GAMES=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE username='$USERNAME'")
   if [[ -z $BEST_GAME || $COUNT_TRIES -lt $BEST_GAME ]]
   then
-   $PSQL "UPDATE users SET best_game=$COUNT_TRIES WHERE username='$USERNAME'"
+   RES_UPDATE_GAMES=$($PSQL "UPDATE users SET best_game=$COUNT_TRIES WHERE username='$USERNAME'")
   fi
-  
+
 
 }
 
